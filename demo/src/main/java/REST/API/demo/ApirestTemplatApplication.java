ef452e83b9bd7350dc5872dc953890e5a288d58d
duizpisico <brenovmsilva82@gmail.com>
2023-10-01 21:15:51 -0300

    project creation

---
+++ demo/src/main/java/REST/API/demo/ApirestTemplatApplication.java
@@ -0,0 +1,13 @@
+package REST.API.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestTemplatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestTemplatApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/REST/API/demo/ApirestTemplatApplicationTests.java
@@ -0,0 +1,13 @@
+package REST.API.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestTemplatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
